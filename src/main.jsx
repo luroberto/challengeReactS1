260fee7c00014c48ace3efc3ec6488d559eed541
luroberto <robertoluciana95@gmail.com>
2024-05-27 20:17:08 -0300

    add material UI

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { CssBaseline } from '@mui/material'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
 )
