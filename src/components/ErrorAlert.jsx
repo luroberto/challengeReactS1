c8377fbe22e1a343cb756205e5dfff433a3fa0ad
luroberto <robertoluciana95@gmail.com>
2024-05-28 10:18:41 -0300

    creating the ErrorAlert component

---
+++ src/components/ErrorAlert.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import { Alert } from "@mui/material"
+
+const ErrorAlert = (props) => {
+    const { message } = props
+    return (
+        <>
+        <Alert severity="error">Ocurrió un error de tipo: {message}</Alert>
+        </>
+    )
+}
+
+export default ErrorAlert
