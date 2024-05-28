bf039cacf8bad76d65e1633e25a47f46c969a9c4
luroberto <robertoluciana95@gmail.com>
2024-05-28 14:33:16 -0300

    center error message

--- src/components/ErrorAlert.jsx
+++ src/components/ErrorAlert.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/prop-types */
 
 // material
-import { Alert } from "@mui/material"
+import { Alert, Box } from "@mui/material"
 
 const ErrorAlert = (props) => {
     const { message } = props
     return (
-        <>
+        <Box display='flex' justifyContent='center'>
         <Alert severity="error">Ocurrió un error de tipo: {message}</Alert>
-        </>
+        </Box>
     )
 }
 
